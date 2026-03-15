06a2d7311412b68b68503a3ebabe202291c6b98c
larsenv <larsen@Weborican.com>
2026-03-15 15:04:51 -0500

    Initialize Sentry for error tracking

---
+++ instrument.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const Sentry = require('@sentry/node');
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    sendDefaultPii: true,
+  });
+}
